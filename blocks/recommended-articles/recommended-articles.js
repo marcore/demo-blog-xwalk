16cb53b806fea7b0c1fe1eba50f12cc7e55f0086
Marco Re <mre@adobe.com>
2025-09-16 16:50:00 +0200

    Improved reco articles

--- blocks/recommended-articles/recommended-articles.js
+++ blocks/recommended-articles/recommended-articles.js
@@ -1,16 +1,26 @@
 import { createElement } from '../../scripts/utils.js';
 
 export default function decorate(block) {
-  const title = block.classList.contains('title');
+  /* const title = block.classList.contains('title');
   if (title) {
     block.children[0].classList.add('title');
+  } */
+  const heading = block.querySelector('h3');
+  if (heading) {
+    const divTitle = createElement('div', { class: 'title' });
+    const titleP = createElement('p');
+    titleP.textContent = heading.textContent;
+    divTitle.append(titleP);
+    heading.replaceWith(divTitle);
+
+    // block.prepend(divTitle);
   }
   /* eslint-disable-next-line no-plusplus */
-  for (let i = 0; i < block.children.length; i++) {
+  for (let i = 1; i < block.children.length; i++) {
     const article = block.children[i];
     if (!article.classList.contains('title')) {
       const numberDiv = createElement('h3', { class: 'article-box-number' });
-      numberDiv.textContent = (title) ? `${i}` : `${i + 1}`;
+      numberDiv.textContent = (heading) ? `${i}` : `${i + 1}`;
       article.prepend(numberDiv);
     }
     const cat = article.querySelector('h6');
